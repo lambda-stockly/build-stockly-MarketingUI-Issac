69649722e6f5ba4f80dcd6e053eb892c40293743
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-23 14:39:11 +0100

    Add image hover effect on horizontal card

--- components/cards/horizontal-cards.js
+++ components/cards/horizontal-cards.js
@@ -5,6 +5,7 @@ class HorizontalCardGenerator {
 
         this.link = this.cardData.link;
         this.imgSrc = this.cardData.imgSrc;
+        this.imgDarkSrc = this.cardData.imgDarkSrc;
         this.title = this.cardData.title;
         this.paragraph = this.cardData.paragraph;
 
@@ -29,10 +30,18 @@ class HorizontalCardGenerator {
     constructImage() {
         const imageContainer = document.createElement('div');
         imageContainer.setAttribute('class', 'work-benefits-image');
+        imageContainer.style.position = 'relative';
+
+        const imageDark = document.createElement('img');
+        imageDark.src = this.imgDarkSrc;
+        imageDark.alt = this.title;
+        imageDark.style.position = 'absolute';
+        imageContainer.append(imageDark);
 
         const image = document.createElement('img');
         image.src = this.imgSrc;
         image.alt = this.title;
+        image.style.position = 'absolute';
         imageContainer.append(image);
 
         this.cardContainer.append(imageContainer);
@@ -64,6 +73,12 @@ class HorizontalCardGenerator {
         let targetedCard = event.target;
         TweenMax.to(targetedCard, 0.3, {position: 'relative', top: '-3px', 
         boxShadow: '0 7px 4px -4px #777', mozBoxShadow: '0 7px 4px -4px #777', webkitBoxShadow: '0 7px 4px -4px #777'});
+        
+        let darkImage = event.target.children[0].children[0];
+        let lightImage = event.target.children[0].children[1]
+
+        TweenMax.to(lightImage, 0.3, {opacity: 0});
+        TweenMax.from(darkImage, 0.3, {opacity: 0});
     }
 
     mouseOutCard(event) {
@@ -71,6 +86,11 @@ class HorizontalCardGenerator {
         TweenMax.to(targetedCard, 0.3, {top: '3px', boxShadow: 'none;', mozBoxShadow: 'none', webkitBoxShadow: 'none'});
         TweenMax.to(targetedCard, {delay: 0.3, position: 'static'});
 
+        let darkImage = event.target.children[0].children[0];
+        let lightImage = event.target.children[0].children[1]
+
+        TweenMax.to(lightImage, 0.3, {opacity: 1});
+        TweenMax.to(darkImage, 0.3, {opacity: 1});
     }
 }
 
@@ -79,12 +99,14 @@ if (document.title == 'About') {
         {
             link: '#',
             imgSrc: 'img/work-benefits/life-at-stockly.png',
+            imgDarkSrc: 'img/work-benefits/life-at-stockly-dark.png',
             title: 'Life at Σtockly →',
             paragraph: "We’re building a place where we can be happy, productive, and do meaningful work."
         },
         {
             link: '#',
             imgSrc: 'img/work-benefits/operate-globally.png',
+            imgDarkSrc: 'img/work-benefits/operate-globally-dark.png',
             title: 'How We Work →',
             paragraph: "We're remote-first, spanning from Europe to the United States."
         }
